931b56f494086d2c94e4a0187d494d729c205e0d
MohitGupta2004 <mohit0907gupta@gmail.com>
2025-05-11 13:10:18 +0530

    Updated Task Controller

--- src/main/java/com/assessment/bajaj/controller/TaskController.java
+++ src/main/java/com/assessment/bajaj/controller/TaskController.java
@@ -1,47 +1,27 @@
 package com.assessment.bajaj.controller;
-
 import com.assessment.bajaj.Dto.RequestDto;
 import com.assessment.bajaj.Dto.ResponseDto;
 import com.assessment.bajaj.service.TaskService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/bhfl")
 public class TaskController {
 
     private final TaskService taskService;
 
-    @Autowired
-    public TaskController(TaskService taskService) {
-        this.taskService = taskService;
-    }
-
- @PostMapping
-    public ResponseEntity<?> task(@RequestBody RequestDto request, HttpServletResponse response, HttpServletRequest req) {
-        response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
-        response.setHeader("Pragma", "no-cache");
-        response.setDateHeader("Expires", 0);
-        System.out.println("POST HIT ✅ method: " + req.getMethod());
+    @PostMapping
+    public ResponseEntity<ResponseDto> task(@RequestBody RequestDto request) {
         return ResponseEntity.ok(taskService.task(request));
     }
 
     @GetMapping
-    public ResponseEntity<?> get(HttpServletResponse response, HttpServletRequest req) {
-        response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
-        response.setHeader("Pragma", "no-cache");
-        response.setDateHeader("Expires", 0);
-        System.out.println("GET HIT ✅ method: " + req.getMethod());
+    public ResponseEntity<Map<String, Integer>> get() {
         return ResponseEntity.ok(Map.of("operation_code", 1));
     }
 }
-}
+

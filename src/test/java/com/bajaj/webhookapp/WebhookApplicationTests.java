07c9393f421e8a340cce2efdc72833ea5302f873
MohitGupta2004 <mohit0907gupta@gmail.com>
2025-05-11 16:55:48 +0530

    Final commit

---
+++ src/test/java/com/bajaj/webhookapp/WebhookApplicationTests.java
@@ -0,0 +1,8 @@
+package com.bajaj.webhookapp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WebhookApplicationTests {
+	// No test cases as per instructions
+}
